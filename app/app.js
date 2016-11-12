28e2e875dac12ef4761cb27ac2f17fad897e6156
nwpray <nwpray@hotmail.com>
2016-11-11 21:31:44 -0700

    Initial Commit, Initial Angular Setup

---
+++ app/app.js
@@ -0,0 +1,8 @@
+/**
+ * NI Directives App
+ *
+ * @author : Nick Pray <nwpray@hotmail.com>
+ * @date : 11/11/2016
+ *
+ */
+angular.module('app', ['ngRoute']);
---
+++ app/routes.js
@@ -0,0 +1,24 @@
+angular.module('app')
+.run
+(
+    function($routeProvider, $locationProvider)
+    {
+        $routeProvider
+        .when
+        (
+            '/',
+            {
+                templateUrl : '/app/pages/home/home.html',
+                controller : 'HomeController',
+                controllerAs : 'Home'
+            }
+        )
+        .otherwise({redirectTo : '/'});
+
+        //Removes # from url. To be able to enter urls without # you must also use .htaccess file.
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+    }
+);
